a508eefb148f8d4dcf37d74b890974b901cd125b
Juanjo200701 <juanjolopin@gmail.com>
2025-03-28 12:23:38 -0500

    Cambio en la página de reseñas con algunas funcionalidades

---
+++ js/comentarios.js
@@ -0,0 +1,177 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let estrella1 = document.getElementById('star1');
+    let estrella2 = document.getElementById('star2');
+    let estrella3 = document.getElementById('star3');
+    let estrella4 = document.getElementById('star4');
+    let estrella5 = document.getElementById('star5');
+
+    estrella5.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+        estrella5.style.color = 'orange';
+    });
+    estrella5.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+        estrella5.style.color = 'gray';
+    });
+    estrella4.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+    })
+    estrella4.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+    })
+    estrella3.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+    })
+    estrella3.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+    })
+
+    estrella2.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+    })
+    estrella2.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+    })
+    estrella1.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+    })
+    estrella1.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+    })
+});
+document.addEventListener('DOMContentLoaded', function() {
+    let estrella1 = document.getElementById('starY1');
+    let estrella2 = document.getElementById('starY2');
+    let estrella3 = document.getElementById('starY3');
+    let estrella4 = document.getElementById('starY4');
+    let estrella5 = document.getElementById('starY5');
+
+    estrella5.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+        estrella5.style.color = 'orange';
+    });
+    estrella5.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+        estrella5.style.color = 'gray';
+    });
+    estrella4.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+    })
+    estrella4.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+    })
+    estrella3.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+    })
+    estrella3.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+    })
+
+    estrella2.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+    })
+    estrella2.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+    })
+    estrella1.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+    })
+    estrella1.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+    })
+});
+document.addEventListener('DOMContentLoaded', function() {
+    let estrella1 = document.getElementById('starX1');
+    let estrella2 = document.getElementById('starX2');
+    let estrella3 = document.getElementById('starX3');
+    let estrella4 = document.getElementById('starX4');
+    let estrella5 = document.getElementById('starX5');
+
+    estrella5.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+        estrella5.style.color = 'orange';
+    });
+    estrella5.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+        estrella5.style.color = 'gray';
+    });
+    estrella4.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+        estrella4.style.color = 'orange';
+    })
+    estrella4.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+        estrella4.style.color = 'gray';
+    })
+    estrella3.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+        estrella3.style.color = 'orange';
+    })
+    estrella3.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+        estrella3.style.color = 'gray';
+    })
+
+    estrella2.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+        estrella2.style.color = 'orange';
+    })
+    estrella2.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+        estrella2.style.color = 'gray';
+    })
+    estrella1.addEventListener('mouseover', function() {
+        estrella1.style.color = 'orange';
+    })
+    estrella1.addEventListener('mouseout', function() {
+        estrella1.style.color = 'gray';
+    })
+});
